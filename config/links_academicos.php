3cbe77a2a455a3bcdd8ea5348165ca949a1b665a
Erickson Zanon <ezanon@gmail.com>
2026-08-21 10:00:40 -0300

    pequenas correções

--- config/links_academicos.php
+++ config/links_academicos.php
@@ -12,7 +12,7 @@ return [
             'icone' => 'iconesacademicos/orcid.png',
             'nome_exibicao' => 'ORCID',
         ],
-        'google-scholar' => [
+        'google_scholar' => [
             'base_url' => 'https://scholar.google.com/citations?user=',
             'icone' => 'iconesacademicos/google_scholar.png',
             'nome_exibicao' => 'Google Scholar',
@@ -22,7 +22,7 @@ return [
             'icone' => 'iconesacademicos/scopus.png',
             'nome_exibicao' => 'Scopus',
         ],
-        'researchid' => [
+        'researcher_id' => [
             'base_url' => 'https://publons.com/researcher/',
             'icone' => 'iconesacademicos/researcher_id.jpg',
             'nome_exibicao' => 'ResearchID',
@@ -32,7 +32,7 @@ return [
             'icone' => 'iconesacademicos/researchgate.jpeg',
             'nome_exibicao' => 'ResearchGate',
         ],
-        'bv-fapesp' => [
+        'bv_fapesp' => [
             'base_url' => 'https://bv.fapesp.br/pt/pesquisador/',
             'icone' => 'iconesacademicos/bv_fapesp.jpg',
             'nome_exibicao' => 'BV FAPESP',
